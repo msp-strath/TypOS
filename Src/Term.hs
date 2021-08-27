b691712e64f5339c60ad6a01d678da1420112ce4
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2021-08-27 17:44:12 +0100

    mangling machinery

--- Src/Term.hs
+++ Src/Term.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE
   DeriveTraversable,
   FlexibleInstances,
   TypeSynonymInstances,
   LambdaCase,
-  TupleSections
+  TupleSections,
+  MultiParamTypeClasses
   #-}
 
 module Term where
@@ -26,9 +28,12 @@ data Tm m
   = V
   | A String
   | P (RP (Tm m) (Tm m))
-  | (Hide String, Bool) :. Tm m
+  | Named Bool :. Tm m
   | m :$ Sbst m
-  deriving (Show, Eq, Ord{-, Functor, Foldable, Traversable-})
+  deriving (Show, Eq, Ord)
+
+data Named a = Hide String := a
+  deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
 instance Traversable Tm where
   traverse f V = pure V
@@ -36,10 +41,66 @@ instance Traversable Tm where
   traverse f (P ((s, th) :<>: (t, ph))) =
      P <$> ((:<>:) <$> ((, th) <$> traverse f s) <*> ((, ph) <$> traverse f t))
   traverse f (xb :. t) = (xb :.) <$> traverse f t
-  traverse f (m :$ (sg, w)) =
-    (:$) <$> f m <*> ((, w) <$> traverse f sg)
+  traverse f (m :$ (sg :^^ w)) =
+    (:$) <$> f m <*> ((:^^ w) <$> traverse f sg)
 instance Functor Tm where fmap = fmapDefault
 instance Foldable Tm where foldMap = foldMapDefault
+
+data Mangler m f = Mangler
+  { mangSrc :: Int               -- size of source scope
+  , mangTgt :: Int               -- size of target scope
+  , mangTh :: Maybe Th           -- are you just embedding src into tgt?
+  , mangV :: f (CdB (Tm m))      -- how to replace variables
+  , mangB :: Mangler m f         -- how to mangle under a relevant binder
+  , mangM :: m -> f (CdB (Tm m)) -- how to replace a meta
+  , mangW :: Int -> Mangler m f  -- how to undo some `mangB`s
+  , mangSelFrom :: Th                -- big end should be mangSrc
+                -> Mangler m f       -- source of output mangler
+                                     --   should be the wee end of the input
+                                     --   thinnings; target stays put
+  }
+
+{-
+stanMangler :: Ord m => Map.Map m (CdB (Tm m)) -> Mangler m (Writer Any)
+stanMangler tbl = mu where
+  mu = Mangler
+         { mangI = null tbl
+         , mangV = pure V
+         , mangB = mu
+         , mangM = _
+         , mangW = const mu
+         , mangSelFrom = _
+         }
+-}
+
+class Manglable m t where
+  mangle  :: Applicative f => Mangler m f -> t -> f (CdB t)
+  -- mangle' is worker for mangle
+  mangle' :: Applicative f => Mangler m f -> t -> f (CdB t)
+  -- Int is size of the target scope
+  mangleCdB :: Applicative f => Mangler m f -> CdB t -> f (CdB t)
+
+  mangle mu t = case mangTh mu of
+    Just th -> pure (t, th)
+    Nothing -> mangle' mu t
+
+  mangleCdB mu (t, th) = mangle mu' t where
+    -- we recheck for mangI after doing a selection computing m'
+    mu' = mangSelFrom mu th
+
+instance Manglable m a => Manglable m (Named a) where
+  mangle' mu (x := a) = ((x :=) $^) <$> mangle' mu a
+
+instance Manglable m (Tm m) where
+  mangle' mu V = mangV mu
+  mangle' mu (A a) = pure (atom a (mangTgt mu))
+  mangle' mu (P p) = (P $^) <$> mangle' mu p
+  mangle' mu ((Hide x := False) :. t) = (x \\) <$> (weak <$> mangle' mu t)
+  mangle' mu ((Hide x := True) :. t) = (x \\) <$> mangle' (mangB mu) t
+  mangle' mu (m :$ s) = (//^) <$> mangM mu m <*> mangle' mu s
+
+instance (Manglable m a, Manglable m b) => Manglable m (RP a b) where
+  mangle' mu (a :<>: b)  = (<&>) <$> mangleCdB mu a <*> mangleCdB mu b
 
 newtype Meta = Meta [(String, Int)]
   deriving (Show, Ord, Eq)
@@ -53,35 +114,51 @@ infixr 3 :.
 infixr 5 :$
 
 -- relevant substitutions
-type Sbst m =
-  ( Sbst' m  -- what's below the weakenings
-  , Int      -- how many weakenings
-  )
+data Sbst m = (:^^)
+  (Sbst' m)  -- what's below the weakenings
+  Int      -- how many weakenings
+  deriving (Show, Eq, Ord)
+
 data Sbst' m
   = S0 -- empty -> empty
-  | ST (RP (Sbst m) (Hide String, Tm m))
-  deriving (Show, Eq, Ord{-, Functor, Foldable, Traversable-})
+  | ST (RP (Sbst m) (Named (Tm m)))
+  deriving (Show, Eq, Ord)
+
+instance Traversable Sbst where
+  traverse f (sg :^^ w) = (:^^ w) <$> traverse f sg
+instance Functor Sbst where fmap = fmapDefault
+instance Foldable Sbst where foldMap = foldMapDefault
 
 instance Traversable Sbst' where
   traverse f S0 = pure S0
-  traverse f (ST (((sg, w), th) :<>: ((x, t), ph))) = ST <$>
-    ((:<>:) <$> ((, th) <$> ((, w) <$> traverse f sg))
-            <*> ((, ph) <$> ((x,) <$> traverse f t)))
+  traverse f (ST ((sg, th) :<>: (t, ph))) = ST <$>
+    ((:<>:) <$> ((, th) <$> traverse f sg)
+            <*> ((, ph) <$> traverse (traverse f) t))
 instance Functor Sbst' where fmap = fmapDefault
 instance Foldable Sbst' where foldMap = foldMapDefault
 
+instance Manglable m (Sbst m) where
+  mangle' mu (sg :^^ w) = sbstW <$> sg' <*> pure (ones w) where
+    mu' = mangW mu w
+    sg' = case sg of
+      S0 -> pure (sbst0 (mangTgt mu'))
+      ST (sg :<>: t) -> sbstT <$> mangleCdB mu' sg <*> mangleCdB mu' t
+
 -- smart constructors
 sbst0 :: Int -> CdB (Sbst m)
-sbst0 de = ((S0, 0), none de)
+sbst0 de = ((S0 :^^ 0), none de)
+
 sbstW :: CdB (Sbst m) -> Th -> CdB (Sbst m)
-sbstW ((sg, w), th) ph = ((sg, w + weeEnd ph), th <> ph)
-sbstT :: CdB (Sbst m) -> CdB (Hide String, Tm m) -> CdB (Sbst m)
-sbstT sg t = ((ST p, 0), ps) where (p, ps) = sg <&> t
+sbstW ((sg :^^ w), th) ph = ((sg :^^ (w + weeEnd ph)), th <> ph)
+
+sbstT :: CdB (Sbst m) -> CdB (Named (Tm m)) -> CdB (Sbst m)
+sbstT sg t = ((ST p :^^ 0), ps) where (p, ps) = sg <&> t
+
 sbstI :: Int -> CdB (Sbst m)
-sbstI w = ((S0, w), ones w)
+sbstI w = ((S0 :^^ w), ones w)
 
 sbstCod :: Sbst m -> Int
-sbstCod (sg, w) = case sg of
+sbstCod (sg :^^ w) = case sg of
   S0 -> w
   ST ((sg, th) :<>: (t, ph)) -> bigEnd th + w
 
@@ -89,8 +166,8 @@ sbstSel
   :: Th -- ga0 from ga
   -> Sbst m -- ga -> de
   -> CdB (Sbst m)
-sbstSel th (S0, w) = ((S0, weeEnd th), th) -- w = bigEnd th
-sbstSel th (ST ((sg, phl{- del <= de -}) :<>: t), w) =
+sbstSel th (S0 :^^ w) = ((S0 :^^ weeEnd th), th) -- w = bigEnd th
+sbstSel th (ST ((sg, phl{- del <= de -}) :<>: t) :^^ w) =
   sbstW (if b then sbstT sg0 t else sg0) thw
   where
   -- ga, x, w -> de, w
@@ -113,23 +190,23 @@ euclid x y = let d = x - y in case d < 0 of
   (sg, ps) -> (t // sg, ps <^> ph)
 
 (//) :: Tm m -> Sbst m -> Tm m
-t // (S0, _) = t
-V // (ST (_ :<>: ((_, t), _)), 0) = t
+t // (S0 :^^ _) = t
+V // (ST (_ :<>: ((_ := t), _)) :^^ 0) = t
 P ((tl, thl) :<>: (tr, thr)) // sg =
   P ((tl // sgl, phl) :<>: (tr // sgr, phr)) where
   (sgl, phl) = sbstSel thl sg
   (sgr, phr) = sbstSel thr sg
-((x, b) :. t) // (sg, w) = (x, b) :. (t // (sg, if b then w+1 else w))
+((x := b) :. t) // (sg :^^ w) = (x := b) :. (t // (sg :^^ if b then w+1 else w))
 (m :$ rh) // sg = m :$ (rh /// sg)
 
 (///) :: Sbst m -> Sbst m -> Sbst m
-(S0, _) /// sg = sg
-rh /// (S0, _) = rh
-(rh, v) /// (sg, w) =
+(S0 :^^ _) /// sg = sg
+rh /// (S0 :^^ _) = rh
+(rh :^^ v) /// (sg :^^ w) =
   case euclid w v of
     LtBy d -> case sg of
       ST ((sg, phl) :<>: t) ->
-        (ST (((rh, d-1) /// sg, phl) :<>: t), w)
+        (ST (((rh :^^ (d-1)) /// sg, phl) :<>: t) :^^ w)
       {-
           -------  ;  -------
              w           w
@@ -140,26 +217,26 @@ rh /// (S0, _) = rh
              rh
       -}
     GeBy d -> case rh of
-      ST ((rh, thl) :<>: ((x, s), thr)) -> let
-        (sgl, phl) = sbstSel thl (sg, d)
-        (sgr, phr) = sbstSel thr (sg, d)
-        in (ST ((rh /// sgl, phl) :<>: ((x, s // sgr), phr)), v)
+      ST ((rh, thl) :<>: ((x := s), thr)) -> let
+        (sgl, phl) = sbstSel thl (sg :^^ d)
+        (sgr, phr) = sbstSel thr (sg :^^ d)
+        in (ST ((rh /// sgl, phl) :<>: (x := (s // sgr), phr)) :^^ v)
       {-
         -------  ;  -------
-           v           v   
+           v           v
         -------     -------
         ----> s     -------
-           rh          d   
+           rh          d
                     -------
-                       sg  
+                       sg
       -}
 
 
 -- toplevel expansions and contractions of co-deBruijn terms
 
 data Xn m
-  = VX Int
-  | AX String
+  = VX Int Int    -- which free variable out of how many?
+  | AX String Int -- how many free variables?
   | CdB (Tm m) :%: CdB (Tm m)
   | String :.: CdB (Tm m)
   | m :$: CdB (Sbst m)
@@ -167,50 +244,50 @@ data Xn m
 
 expand :: CdB (Tm m) -> Xn m
 expand (t, th) = case t of
-  V   -> VX (lsb th)
-  A a -> AX a
+  V   -> VX (lsb th) (bigEnd th)
+  A a -> AX a (bigEnd th)
   P (s :<>: t) -> (s *^ th) :%: (t *^ th)
-  (str, b) :. t -> unhide str :.: (t, th -? b)
+  (str := b) :. t -> unhide str :.: (t, th -? b)
   f :$ sg -> f :$: (sg, th)
 
 (?:) :: CdB (Tm m) -> (Xn m -> a) -> a
 t ?: f = f (expand t)
 
-contract :: Xn m -> Int -> CdB (Tm m)
-contract t ga = case t of
-  VX x -> (V, inx (x, ga))
-  AX a -> (A a, none ga)
+contract :: Xn m -> CdB (Tm m)
+contract t = case t of
+  VX x ga -> (V, inx (x, ga))
+  AX a ga -> (A a, none ga)
   s :%: t -> P $^ (s <&> t)
   x :.: (t, th) -> case thun th of
-    (th, b) -> ((Hide x, b) :. t, th)
+    (th, b) -> ((Hide x := b) :. t, th)
   m :$: sg -> (m :$) $^ sg
 
--- smart constructors for the codeBruijn terms
+-- smart constructors for the codeBruijn terms; bigEnds must agree
 
 var :: Int -> Int -> CdB (Tm m)
-var x = contract (VX x)
+var x ga = contract (VX x ga)
 
 atom :: String -> Int -> CdB (Tm m)
-atom a = contract (AX a)
+atom a ga = contract (AX a ga)
 
 nil :: Int -> CdB (Tm m)
 nil = atom ""
 
 infixr 4 %
-(%) :: (Int -> CdB (Tm m)) -> (Int -> CdB (Tm m)) -> Int -> CdB (Tm m)
-(s % t) ga = contract (s ga :%: t ga) ga
+(%) :: CdB (Tm m) -> CdB (Tm m) -> CdB (Tm m)
+s % t = contract (s :%: t)
 
-(#%) :: String -> [Int -> CdB (Tm m)] -> Int -> CdB (Tm m)
-(a #% ts) ga = case foldr (%) nil ts ga of
+(#%) :: (String, Int) -> [CdB (Tm m)] -> CdB (Tm m)
+(a, ga) #% ts = case foldr (%) (nil ga) ts of
   (t, th) -> (P (atom a ga :<>: (t, ones (weeEnd th))), th)
 
 infixr 3 \\
-(\\) :: String -> (Int -> CdB (Tm m)) -> Int -> CdB (Tm m)
-(x \\ t) ga = contract (x :.: t (ga+1)) ga
+(\\) :: String -> CdB (Tm m) -> CdB (Tm m)
+x \\ t = contract (x :.: t)
 
 infixr 5 $:
-($:) :: m -> (Int -> CdB (Sbst m)) -> Int -> CdB (Tm m)
-(m $: sg) ga = contract (m :$: sg ga) ga
+($:) :: m -> CdB (Sbst m) -> CdB (Tm m)
+m $: sg = contract (m :$: sg)
 
 
 -- patterns are de Bruijn
@@ -231,8 +308,8 @@ match r (MP x ph) (t, th) = do
   let (m, r') = meta r x
   return (r', (Map.singleton x m, Map.singleton m (t, ps)))
 match r p t = case (p, expand t) of
-  (VP i, VX j) | i == j -> return (r, (Map.empty, Map.empty))
-  (AP a, AX b) | a == b -> return (r, (Map.empty, Map.empty))
+  (VP i, VX j _) | i == j -> return (r, (Map.empty, Map.empty))
+  (AP a, AX b _) | a == b -> return (r, (Map.empty, Map.empty))
   (PP p q, s :%: t) -> do
     (r, m) <- match r p s
     (r, n) <- match r q t
@@ -269,7 +346,7 @@ display (B0, _, _)    (A a) = case a of
   _  -> '\'' : a
 display na (P (s :<>: (t, ph))) =
   "[" ++ display' na s ++ displayCdr (nameSel ph na) t ++ "]"
-display na ((Hide x, b) :. t) = "\\" ++ case b of
+display na ((Hide x := b) :. t) = "\\" ++ case b of
   False -> "_." ++ display na t
   True  -> let y = freshen x na in
     y ++ "." ++ display (nameOn na y) t
@@ -287,18 +364,18 @@ displayCdr na (P (s :<>: (t, ph))) =
 displayCdr na t = "|" ++ display na t
 
 displaySg :: Show m => Naming -> Sbst m -> [String]
-displaySg (_, th, _) (S0, _)
+displaySg (_, th, _) (S0 :^^ _)
   | th == ones (bigEnd th) = []
-displaySg na (ST ((sg, th) :<>: ((Hide x, t), ph)), 0) =
+displaySg na (ST ((sg, th) :<>: ((Hide x := t), ph)) :^^ 0) =
   (x ++ "=" ++ display' na (t, ph)) :
   displaySg (nameSel th na) sg
-displaySg (xz, th, yz :< y) (sg, w) = case thun th of
+displaySg (xz, th, yz :< y) (sg :^^ w) = case thun th of
   (th, False) ->
-    (y ++ "*") : displaySg (xz, th, yz) (sg, w)
+    (y ++ "*") : displaySg (xz, th, yz) (sg :^^ w)
   (th, True) ->
     case xz of
       xz :< x ->
-        x : displaySg (xz, th, yz) (sg, w - 1)
+        x : displaySg (xz, th, yz) (sg :^^ (w - 1))
 
 
 
