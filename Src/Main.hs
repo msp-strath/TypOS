05d33b94f48b8c37d4418ed7cb6316d3605caa1f
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2022-03-04 17:03:21 +0000

    delete unused and bitrotten files

--- Src/Main.hs
+++ Src/Main.hs
@@ -1,105 +1,153 @@
-{-# LANGUAGE FlexibleInstances #-}
-
 module Main where
 
-import qualified Data.Map as Map
+import Control.Monad.Except
+import Control.Monad.Reader
 
-import Data.Maybe
 import Data.Foldable
-import Control.Monad.State
+import Data.Traversable
 
-import Actor
 import Bwd
+import Elaboration
 import Parse
+import qualified Actor as A
+import Actor.Display (DAEnv, initDAEnv, declareChannel)
+import Machine hiding (declareChannel)
 import Term
-import Thin
 import Display
+
+
+import qualified Concrete.Base as C
+import Concrete.Parse
+
 import Syntax
-import Command
 
-data Mode = Input | Subject | Output deriving (Eq, Show)
+import System.Environment
+import Control.Applicative
 
-data SystemState = SystemState
-  { syntaxTable :: Map.Map SyntaxCat SyntaxDesc
-  , judgementTable :: Map.Map JudgementForm [(SyntaxCat,Mode)]
-  }
-  deriving Show
+data CommandF jd ch t a
+  = DeclJ jd [(Mode, SyntaxCat)]
+  | DefnJ (jd, ch) a
+  | DeclS [(SyntaxCat, t)]
+  | Go a
+  | Trace [MachineStep]
+  deriving (Show)
+
+type CCommand = CommandF C.Variable C.Variable C.Raw C.Actor
+type ACommand = CommandF A.JudgementForm A.Channel ACTm A.Actor
+
+instance Display Mode where
+  type DisplayEnv Mode = ()
+  display Input = pure "?"
+  display Output = pure "!"
+
+instance Display Protocol where
+  type DisplayEnv Protocol = ()
+  display p = (fold <$>) $ for p $ \ (m, c) -> do
+    m <- display m
+    pure $ m ++ c ++ ". "
+
+instance Display String where
+  type DisplayEnv String = ()
+  display str = pure str
+
+instance ( Display0 jd, Display0 ch
+         , Show t -- , Display t, Forget Naming (DisplayEnv t)
+         , Display a, DisplayEnv a ~ DAEnv) =>
+         Display (CommandF jd ch t a) where
+  type DisplayEnv (CommandF jd ch t a) = ()
+  display = \case
+    DeclJ jd p -> do
+      jd <- display jd
+      p <- display p
+      pure $ unwords [ jd, ":", p]
+    d@(DefnJ (jd, ch) a) -> do
+      jd <- display jd
+      ch <- display ch
+      -- hack: the above happens to convert ch into a string, ready to be declared
+      a <- withEnv (declareChannel (A.Channel ch) initDAEnv) $ display a
+      pure $ unwords [ jd, "@", ch, "=", a]
+    DeclS _ -> pure ""
+    Go a -> withEnv initDAEnv $ display a
+    Trace ts -> pure ""
+
+pmachinestep :: Parser MachineStep
+pmachinestep =
+  MachineRecv <$ plit "recv"
+  <|> MachineSend <$ plit "send"
+  <|> MachineExec <$ plit "exec"
+  <|> MachineMove <$ plit "move"
+  <|> MachineUnify <$ plit "unify"
+  <|> MachineBreak <$ plit "break"
+
+pmode :: Parser Mode
+pmode = Input <$ pch (== '?') <|> Output <$ pch (== '!')
+
+pjudgeat :: Parser (C.Variable, C.Variable)
+pjudgeat = (,) <$> pnom <* punc "@" <*> pnom
+
+pprotocol :: Parser Protocol
+pprotocol = psep pspc ((,) <$> pmode <* pspc <*> patom <* pspc <* pch (== '.'))
+
+psyntax :: Parser (SyntaxCat, C.Raw)
+psyntax = (,) <$> patom <* punc "=" <*> plocal B0 ptm
+
+pcommand :: Parser CCommand
+pcommand
+    = DeclJ <$> pnom <* punc ":" <*> pprotocol
+  <|> DefnJ <$> pjudgeat <* punc "=" <*> pACT
+  <|> DeclS <$ plit "syntax" <*> pcurlies (psep (punc ";") psyntax)
+  <|> Go <$ plit "exec" <* pspc <*> pACT
+  <|> Trace <$ plit "trace" <*> pcurlies (psep (punc ",") pmachinestep)
+
+pfile :: Parser [CCommand]
+pfile = id <$ pspc <*> psep pspc pcommand <* pspc
+
+collectDecls :: [CCommand] -> Elab Decls
+collectDecls [] = asks declarations
+collectDecls (DeclJ jd p : ccs) = do
+  during (DeclJElaboration jd) $ isFresh jd
+  local (declare jd (AJudgement p)) $ collectDecls ccs
+collectDecls (_ : ccs) = collectDecls ccs
+
+elaborate :: [CCommand] -> Either Complaint [ACommand]
+elaborate ccs = evalElab $ do
+  ds <- collectDecls ccs
+  local (setDecls ds) $ forM ccs $ \case
+    DeclJ jd p -> pure (DeclJ jd p)
+    DefnJ (jd, ch) a -> during (DefnJElaboration jd) $ do
+      ch <- pure (A.Channel ch)
+      resolve jd >>= \case
+        Just (Left (AJudgement p)) -> do
+          withChannel ch p $ DefnJ (jd, ch) <$> sact a
+        Just _ -> throwError (NotAValidJudgement jd)
+        _ -> throwError (OutOfScope jd)
+    DeclS syns -> DeclS <$> traverse (traverse stm) syns
+    Go a -> Go <$> sact a
+    Trace ts -> pure (Trace ts)
 
 
-initState :: SystemState
-initState = SystemState
-  { syntaxTable = Map.fromList [("syntax", syntaxDesc)
-                               ,("command", commandDesc)
-                               ,("mode", modeDesc)
-                               ,("term", termDesc)
-                               ]
-  , judgementTable = Map.singleton "guess" [("term",Output)]
-  }
-
-instance OrBust x => OrBust (State s x) where
-  bust = pure bust
-
-instance OrBust Response where
-  bust = atom "BustError" 0
-
-instance OrBust () where
-  bust = ()
-
-asMode :: OrBust x => (Mode -> x) -> CdB (Tm m) -> x
-asMode f = asAtom $ \case
-  ("Input",_) -> f Input
-  ("Subject",_) -> f Subject
-  ("Output",_) -> f Output
-  _ -> bust
-
-command :: Command -> State SystemState Response
-command c = do
-  st <- gets syntaxTable
-  if not (validate st B0 (rec "command") c)
-    then return (atom "MisformedCommandError" 0)
-    else ($ c) $ asTagged $ \case
-      ("SYNTAX",_) -> asPair $ asList $ \ ss -> asNil $ do
-        traverse updateSyntaxTable ss
-        return (atom "SyntaxAdded" 0)
-      ("PRINT",_) -> asPair $ \ t -> asNil $ return t
-      ("JUDGEMENT",_) -> asPair $ asAtom $ \ (j,_) -> asPair $ asList $ \ ss -> asNil $
-        case traverse (validateJudgementPos st) ss of
-          Nothing ->  pure $ atom "SyntaxCatNotFoundError" 0
-          Just xs -> do modify $ \st -> st { judgementTable = Map.insert j xs (judgementTable st) }
-                        return (atom "JudgementAdded" 0)
-
-      _ -> \ _ -> return (atom "UnknownCommandError" 0)
-  where
-    updateSyntaxTable :: CdB (Tm Meta) -> State SystemState ()
-    updateSyntaxTable = asPair $ asAtom $ \ (name, _) syn -> modify (\ state -> state {syntaxTable = Map.insert name syn (syntaxTable state)})
-
-    validateJudgementPos :: SyntaxTable -> CdB (Tm Meta) -> Maybe (SyntaxCat,Mode)
-    validateJudgementPos tbl = asPair $ asAtom $ \(cat,_) -> asMode $ \ m -> (cat,m) <$ Map.lookup cat tbl
-
-terms :: String -> [Term]
-terms s = case parser (pspc *> ptm <* pnl) B0 s of
-  [(t,s)] -> (fmap shitMeta $^ t):terms s
-  _ -> error ("Unparsed input: " ++ s) `seq` []
-
-
-testing = map fst $ Map.toList $ syntaxTable $ execState (command (fmap shitMeta $^ parse ptm lamSyntax)) initState where
-  lamSyntax
-    = "['SYNTAX '[['chk | ['Tag '[ ['Lam | '[['Bind 'syn ['Rec 'chk]]] ]\
-                                  \['Emb | '[ ['Rec 'syn] ] ] ]]]\
-                 \['syn | ['Tag '[ ['Rad | '[ ['Rec 'chk] ['Rec 'typ] ] ]\
-                                  \['App | '[ ['Rec 'syn] ['Rec 'chk] ] ] ]]]\
-                 \['typ | ['Tag '[ ['Base | '[]] ['Arrow | '[ ['Rec 'typ] ['Rec 'typ] ] ] ]]]\
-      \]]"
-
+run :: Process Store Bwd -> [ACommand] -> Process Store []
+run p [] = exec p
+run p@Process{..} (c : cs) = case c of
+  DefnJ (jd, ch) a -> run (p { stack = stack :< Rules jd (ch, a) }) cs
+  Go a -> -- dmesg (show a) $
+          let (lroot, rroot) = splitRoot root ""
+              rbranch = Process tracing [] rroot env (today store) a ""
+          in run (p { stack = stack :< LeftBranch Hole rbranch, root = lroot}) cs
+  Trace xs -> run (p { tracing = xs ++ tracing }) cs
+  _ -> run p cs
 
 main :: IO ()
 main = do
-  putStrLn "Start typing now"
-  s <- getContents
-  let ts = evalState (traverse command (terms s)) initState
-  for_ ts $ \ t ->
-    putStrLn (display initNaming t)
-
-{-
-test = display' initNaming (fst (head (parser (pspc *> ptm) B0 (display' initNaming syntaxDesc)))) == display' initNaming syntaxDesc
--}
+  args <- getArgs
+  let fp = case args of {(fp :_) -> fp; _ -> "stlc.act"}
+  txt <- readFile fp
+  let ccs = parse pfile txt
+  acs <- case elaborate ccs of
+           Left err -> error (show err)
+           Right acs -> pure acs
+  -- putStrLn $ unsafeEvalDisplay $ collapse <$> traverse display acs
+  let p = Process [] B0 initRoot (A.initEnv B0) initStore A.Win ""
+  let res@(Process _ fs _ env sto A.Win _) = run p acs
+  -- putStrLn $ display initNaming res
+  dmesg "" res `seq` pure ()
