23c0fda5712e7b468b71d98f7a8ef4697de64aa4
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2022-04-04 11:39:34 +0100

    [ main ] Don't use `error`

--- Src/Main.hs
+++ Src/Main.hs
@@ -4,6 +4,8 @@ module Main where
 
 import Data.Maybe
 
+import System.Exit
+
 import ANSI hiding (withANSI)
 import Bwd
 import Concrete.Base
@@ -23,13 +25,13 @@ main = do
   opts <- getOptions
   txt <- readFile (filename opts)
   let ccs = parse pfile txt
-  acs <- case elaborate ccs of
-    Left err -> do error $ render (Config 80 Vertical) $
-                     vcat [ withANSI [ SetColour Background Red ] "Error"
-                          , pretty err ]
-
-    Right acs -> pure acs
+  case elaborate ccs of
+    Left err -> do
+      putStrLn $ render (Config 80 Vertical) $
+        vcat [ withANSI [ SetColour Background Red ] "Error" , pretty err ]
+      exitFailure
+    Right acs -> do
   -- putStrLn $ unsafeEvalDisplay initNaming $ collapse <$> traverse display acs
-  let p = Process (fromMaybe [] (tracingOption opts)) B0 initRoot (initEnv B0) initStore Win ""
-  let res@(Process _ fs _ env sto Win _) = run opts p acs
-  dmesg "" res `seq` pure ()
+      let p = Process (fromMaybe [] (tracingOption opts)) B0 initRoot (initEnv B0) initStore Win ""
+      let res@(Process _ fs _ env sto Win _) = run opts p acs
+      dmesg "" res `seq` pure ()
