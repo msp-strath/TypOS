73955ba84a9d19e4a52a2e301ea284b07d304dcf
G. Allais <guillaume.allais@ens-lyon.org>
2022-05-09 12:18:36 +0100

    [ emacs ] highlight error source in buffer (#20)

--- emacs/typos.el
+++ emacs/typos.el
@@ -1,4 +1,3 @@
-
 (require 'compile)
 
 ;; based on: http://ergoemacs.org/emacs/elisp_syntax_coloring.html
@@ -88,11 +87,20 @@
             1 (2 . 5) (3 . 6))
            ("^Parse error at location: \\([^[:space:]]*\\):\\([0-9]+\\):\\([0-9]+\\)"
             1 2 (3 . 4))))
-    (add-hook 'compilation-filter-hook 'typos-compilation-filter nil t)
-  ))
+    (add-hook 'compilation-filter-hook 'typos-compilation-filter nil t)))
+
+(defface typos-highlight-error-face
+  '((t (:underline (:color "red" :style wave))))
+  "The face used for errors.")
 
 (defun typos-run-on-file (typos-file options)
   "Run typOS in a compilation buffer on TYPOS-FILE."
+  (setq compilation-auto-jump-to-first-error t)
+  (setq next-error-highlight-timer t)
+  (setq next-error-highlight t)
+  (setq typos-error-highlight (make-overlay (point-min) (point-min)))
+  (overlay-put typos-error-highlight 'face 'typos-highlight-error-face)
+  (setq compilation-highlight-overlay typos-error-highlight)
   (save-some-buffers compilation-ask-about-save
                      (when (boundp 'compilation-save-buffers-predicate)
                        compilation-save-buffers-predicate))
