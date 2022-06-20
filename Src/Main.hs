597d627f484f3c5c026ab3a9894359d150be62fd
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-06-20 14:54:54 +0100

    [ new ] warn about unsolved metas

--- Src/Main.hs
+++ Src/Main.hs
@@ -27,6 +27,9 @@ import Machine.Trace (diagnostic, ldiagnostic, adiagnostic)
 import Utils
 import Display (unsafeEvalDisplay)
 import Location
+import qualified Data.Map as Map
+import Data.Maybe (isNothing)
+import Data.List (intercalate)
 
 main :: IO ()
 main = do
@@ -49,15 +52,32 @@ main = do
   -- putStrLn $ unsafeEvalDisplay initNaming $ collapse <$> traverse display acs
       let p = Process opts B0 initRoot (initEnv B0) initStore (Win unknown) []
       let res@(Process _ fs _ env sto a _) = run opts p acs
+
+      -- Failed run error
       unless (isWin a) $ do
-         putStrLn $ ANSI.withANSI [SetColour Background Red] "Error: Did not win"
+         putStrLn $ ANSI.withANSI [SetColour Background Red] "Error:" ++ " Did not win"
          putStrLn $ let (_, _, _, a) = unsafeEvalDisplay initDEnv $ displayProcess' res in
                     render (colours $ options p) cfg a
-      unless (quiet opts) $ putStrLn $ diagnostic opts sto fs
+
+      -- Unsolved metas warning
+      let unsolved = Map.mapMaybe (\ (_, msol)  -> () <$ guard (isNothing msol)) $ solutions sto
+      case Map.keys unsolved of
+        [] -> pure ()
+        ms -> putStrLn $ ANSI.withANSI [SetColour Background Yellow] "Warning:"
+                      ++ " Unsolved meta" ++ (if length ms > 1 then "s" else "")
+                      ++ " (" ++ intercalate ", " (show <$> ms) ++ ")"
+
+      -- Resultign derivation
+      unless (quiet opts) $ do
+        putStrLn $ diagnostic opts sto fs
+
+      -- LaTeX & beamer backends
       whenJust (latex opts) $ \ file -> do
         writeFile file $ ldiagnostic table sto fs
-        putStrLn (ANSI.withANSI [ SetColour Background Green ] "Success:" ++ " wrote latex derivation to " ++ file)
+        putStrLn $ ANSI.withANSI [ SetColour Background Green ] "Success:"
+                ++ " wrote latex derivation to " ++ file
       whenJust (latexAnimated opts) $ \ file -> do
         writeFile file $ adiagnostic table sto fs (logs res)
-        putStrLn (ANSI.withANSI [ SetColour Background Green ] "Success:" ++ " wrote animated latex derivation to " ++ file)
+        putStrLn $ ANSI.withANSI [ SetColour Background Green ] "Success:"
+                ++ " wrote animated latex derivation to " ++ file
       dmesg "" res `seq` pure ()
