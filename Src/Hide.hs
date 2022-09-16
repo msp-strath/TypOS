c9c17881246658e0945005b85219f44a2a0f87cb
Jacques Carette <carette@mcmaster.ca>
2022-09-16 16:58:48 +0100

    Document Hide.hs

--- Src/Hide.hs
+++ Src/Hide.hs
@@ -1,12 +1,21 @@
 {-# LANGUAGE DeriveTraversable #-}
 
+{-|
+Description: Hiding a value by making it behave like a singleton
+-}
+
 module Hide where
 
+-- | Hide adds a layer above a value that lets us make it look
+-- like a singleton, but not forget the actual value.
 newtype Hide x = Hide {unhide :: x}
   deriving (Functor)
 instance Show (Hide x) where show _ = ""
 instance Eq   (Hide x) where _ == _ = True
 instance Ord  (Hide x) where compare _ _ = EQ
 
+-- | Something is a 'Named' value of type 'a' if it is a pair of a
+-- hidden String and a value of type 'a'. The value part is completely
+-- transparent, i.e. treats the (hidden) string as an annotation.
 data Named a = Hide String := a
   deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
