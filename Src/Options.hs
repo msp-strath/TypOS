7bd85e50a2364322e5045fab5c92160928f7bd55
Jacques Carette <carette@mcmaster.ca>
2022-11-02 17:03:43 +0000

    finish documenting Options

--- Src/Options.hs
+++ Src/Options.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {- | Description: The various options that can be given on the command line
 
+  Also stores whether the terminal is dumb, which is an "environment option"
 -}
 module Options where
 
@@ -11,7 +12,7 @@ import System.Console.Terminal.Size (size, width)
 import System.Environment (getEnv)
 
 import qualified ANSI
-import Machine.Steps (MachineStep(..))
+import Machine.Steps (MachineStep(..), readSteps, tracingHelp)
 import Pretty (Annotations,toANSIs)
 import qualified Text.PrettyPrint.Compact as Compact
 
@@ -43,6 +44,7 @@ unsafeOptions = Options
  , noContext = False
  }
 
+-- | Parse our options.
 poptions :: Parser Options
 poptions = Options
   <$> argument str (metavar "FILE" <> completer (bashCompleter "file") <> help "Actor file")
@@ -56,6 +58,7 @@ poptions = Options
   <*> pure 80 -- dummy value
   <*> flag False True (long "no-context" <> help "Do not print file context of errors")
 
+-- | Actually get the actions
 getOptions :: IO Options
 getOptions = do
   opts <- execParser (info (poptions <**> helper)
@@ -65,9 +68,11 @@ getOptions = do
   let w = maybe 80 width termSize
   pure $ opts { termWidth = w }
 
+-- | Is the terminal in which we're currently running "dumb" ?
 isTermDumb :: IO Bool
 isTermDumb = ("dumb"==) <$> getEnv "TERM"
 
+-- | for creating the first argument to 'renderWith'
 renderOptions :: Options -> Compact.Options Annotations String
 renderOptions opts = Compact.Options
   { optsPageWidth = termWidth opts
