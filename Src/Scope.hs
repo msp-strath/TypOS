e237d80d471c5f4021f42109a165c02bd02603a2
Jacques Carette <carette@mcmaster.ca>
2022-12-09 17:01:51 +0000

    [doc] Scope.hs (#63)
    * [doc] Format
    
    * [doc] Scope.hs
    
    Co-authored-by: G. Allais <guillaume.allais@ens-lyon.org>

--- Src/Scope.hs
+++ Src/Scope.hs
@@ -1,14 +1,19 @@
 module Scope where
+{- Description: Defining 'Scope'. A scope is a pair of a hidden value (a bound name) in a "term"
+ -}
 
-import Hide
-import Thin
+import Hide (Hide(unhide))
+import Thin (Thable(..), (-?))
 
 -- TODO: refactor Tm using Scope?
+-- | Definition of Scope
 data Scope x t = Scope (Hide x) t
   deriving (Eq, Show)
 
+-- | extract the bound name from a 'Scope'
 boundName :: Scope x t -> x
-boundName (Scope (Hide x) _) = x
+boundName (Scope x _) = unhide x
 
+-- | Scopes are thinable (by adding a bit at the end)
 instance Thable t => Thable (Scope x t) where
   Scope x t *^ th = Scope x (t *^ (th -? True))
